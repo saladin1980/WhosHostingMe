4b97adf3fcfaa74a18bd592870eda7395b986a67
Saladin <Saladin@MAIN-PC>
2016-06-18 17:24:31 -0400

    Add project files.

---
+++ WhosHostingMe/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WhosHostingMe
+{
+    partial class whoisHostingme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(whoisHostingme));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Twitch Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 19);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 225);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "How many :";
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(134, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(119, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 112);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            this.contextMenuStrip1.Text = "Copy";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.copyToolStripMenuItem.Text = "copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // whoisHostingme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(230, 288);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "whoisHostingme";
+            this.Text = "Whos Hosting ...?";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WhosHostingMe/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace WhosHostingMe
+{
+    public partial class whoisHostingme : Form
+    {
+
+        // Default VARS
+        static string twitchName = "saladin1980";
+        static string id = null;
+
+        public whoisHostingme()
+        {
+            
+
+            InitializeComponent();
+            
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            label3.Text = "v." + version;
+            getID(twitchName);
+            getHosts(id);
+        }
+
+        public static void getID(string tn = "saladin1980")
+        {
+
+            string URL = "https://api.twitch.tv/kraken/users/"+ tn;
+            using (var webClient = new System.Net.WebClient())
+            {
+                try
+                {
+                    var json2 = webClient.DownloadString(URL);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("error: please advise creater that an error happened on line:38");
+                    MessageBox.Show("Error is: "+e);
+                    MessageBox.Show("Closing the application due to the error stated. Please advise the coder for a resolution.");
+                    Application.Exit();
+                }
+
+                var json = webClient.DownloadString(URL);
+                JsonTextReader reader = new JsonTextReader(new StringReader(json));
+                bool I = false;
+                while (reader.Read())
+                {
+                    if (reader.Value != null)
+                    {
+                        string v = Convert.ToString(reader.Value);
+                        if ("_id" == v)
+                        {
+                            I = true;
+                        }
+                        else if(I)
+                        {
+                            // MessageBox.Show(v);
+                            I = false;
+                            id = v;
+                        }
+                    }
+                }
+            }
+        }
+        private void getHosts(string id)
+        {
+            string URL = "http://tmi.twitch.tv/hosts?include_logins=1&target="+ id;
+            List<string> _items = new List<string>();
+            using (var webClient = new System.Net.WebClient())
+            {
+                string json = webClient.DownloadString(URL);
+                JsonTextReader reader = new JsonTextReader(new StringReader(json));
+                bool I = false;
+                _items = new List<string>();
+                while (reader.Read())
+                {
+                    if (reader.Value != null)
+                    {
+                        Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+                        string v = Convert.ToString(reader.Value);
+                        if ("host_login" == v)
+                        {
+                            I = true;
+                            // MessageBox.Show("ID FOUND");
+                        }
+                        else if (I)
+                        {
+                            _items.Add(v);
+                            I = false;
+                        }
+                    }
+                }
+                listBox1.DataSource = _items;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "Twitch Name") twitchName = "saladin1980";
+            else twitchName = textBox1.Text;
+            listBox1.DataSource = null;
+            getID(twitchName);
+            getHosts(id);
+            textBox2.Text = listBox1.Items.Count.ToString();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string s = null;
+            s = twitchName +" is hosted by: ";
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if(i == 0) s = s + listBox1.Items[i].ToString();
+                if(i>0) s = s + ", " + listBox1.Items[i].ToString();
+                //MessageBox.Show(listBox1.Items[i].ToString());
+            }
+            MessageBox.Show("Copied to your clipboard: " + Environment.NewLine + s);
+            Clipboard.SetText(s);
+        }
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+
+        {
+
+            if (e.Button == MouseButtons.Right)
+
+                contextMenuStrip1.Show(Cursor.Position.X, Cursor.Position.Y);
+
+        }
+    }
+}
